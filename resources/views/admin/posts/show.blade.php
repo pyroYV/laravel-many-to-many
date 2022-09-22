d8073d7088144096ab558a7d1f5edfc70b67a632
pyroYV <simonerizzo.pvt@gmail.com>
2022-09-22 17:07:52 +0200

    Feat: tags show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,8 +3,13 @@
 @section('content')
 <div class="container-lg">
     <div class="row">
-        <div class="col-12">
+        <div class="col-11">
         <h2>{{$post->post_title}}</h2><h4>Author: {{$post->post_author}}</h4>
+        </div>
+        <div class="col-1 my-2">
+            @foreach ($post->tags as $tag)
+            <button class="btn btn-warning my-1">{{$tag->name}}</button>
+            @endforeach
         </div>
         <div class="col-12">
             <img src="{{$post->post_image}}" alt="image of {{ $post->post_title}}" class="w-100">
